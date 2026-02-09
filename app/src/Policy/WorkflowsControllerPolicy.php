0544e34fe13cc1ca8fd4d08401b14deef8407700
Josh Handel <josh@liveoak.ws>
2026-02-09 17:36:04 +0000

    fix: Add WorkflowsControllerPolicy, remove conflicting BasePolicy overrides
    Fixes 'Policy for Workflows has not been defined' warning from nav cell.
    Removes canIndex/canAdd overrides that conflicted with BasePolicy signatures.

---
+++ app/src/Policy/WorkflowsControllerPolicy.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policy;
+
+use App\KMP\KmpIdentityInterface;
+use Authorization\Policy\ResultInterface;
+
+/**
+ * Controller-level policy for WorkflowsController.
+ *
+ * Resolved by ControllerResolver when nav cells check canAccessUrl
+ * for Workflows routes. Only defines methods for actions not already
+ * in BasePolicy (index, add, edit, delete, view are inherited).
+ */
+class WorkflowsControllerPolicy extends BasePolicy
+{
+    public function canApprovals(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canInstances(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canDesigner(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canVersions(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canViewInstance(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canRegistry(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canSave(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canPublish(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canRecordApproval(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canToggleActive(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canCreateDraft(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canCompareVersions(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+
+    public function canMigrateInstances(
+        KmpIdentityInterface $user,
+        array $urlProps,
+    ): ResultInterface|bool {
+        return $this->_hasPolicyForUrl($user, __FUNCTION__, $urlProps);
+    }
+}
